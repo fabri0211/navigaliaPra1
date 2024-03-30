95449eb0546dff4af8c4b43151a9a19d1aa5986a
fabri <fabrisioum@gmail.com>
2024-03-30 12:37:57 +0100

    brujula + animacion mapa

--- Industrious/assets/js/index.js
+++ Industrious/assets/js/index.js
@@ -57,26 +57,51 @@ document.addEventListener("DOMContentLoaded", function () {
         ruta1: "images/ruta-MON.jpg",
         ruta2: "images/ruta-MLL.jpg",
         ruta3: "images/ruta-ROM.jpg",
-        ruta4:  "images/ruta-ATH.jpg",
+        ruta4: "images/ruta-ATH.jpg",
         ruta5: "images/ruta-BCN.jpg"
     };
 
     var imagenBase = document.getElementById("imagenBase");
+    var imagenBaseSrc = imagenBase.src; // Guardar la fuente original de la imagen base
 
-    // Función para restaurar la imagen base
-    function restaurarImagenBase() {
-        imagenBase.src = "images/mapabase.jpg"; // Ruta de la imagen base
+    // Función para cambiar la imagen base
+    function cambiarImagen(ruta) {
+        imagenBase.style.opacity = "0"; // Iniciar transición de opacidad
+        imagenBase.src = ruta; // Cambiar la imagen base
+        setTimeout(function () {
+            imagenBase.style.opacity = "1"; // Restaurar opacidad después de la transición
+        }, 100); // Esperar un breve período antes de restaurar la opacidad
     }
+
+    // Controlador de evento para restaurar la imagen base cuando el cursor está fuera de las rutas
+    document.body.addEventListener("mouseout", function () {
+        cambiarImagen(imagenBaseSrc); // Restaurar la imagen base original
+    });
 
     // Agregar eventos a cada ruta para cambiar la imagen al pasar el cursor sobre ella
     var rutas = document.getElementsByClassName("ruta");
     for (var i = 0; i < rutas.length; i++) {
         rutas[i].addEventListener("mouseover", function () {
             var idRuta = this.id;
-            imagenBase.src = imagenesRutas[idRuta];
+            cambiarImagen(imagenesRutas[idRuta]); // Llamar a la función para cambiar la imagen
         });
-
-        // Restaurar la imagen base al quitar el cursor de encima de la ruta
-        rutas[i].addEventListener("mouseout", restaurarImagenBase);
     }
+});
+
+const imagen = document.getElementById('brujula');
+
+document.addEventListener('mousemove', function(e) {
+  const { clientX, clientY } = e;
+  const { left, top, width, height } = imagen.getBoundingClientRect();
+
+  const centerX = left + width / 2;
+  const centerY = top + height / 2;
+
+  const deltaX = clientX - centerX;
+  const deltaY = clientY - centerY;
+
+  const angle = Math.atan2(deltaY, deltaX);
+  const angleDeg = angle * (180 / Math.PI) - 90;
+
+  imagen.style.transform = `rotate(${angleDeg}deg)`;
 });
